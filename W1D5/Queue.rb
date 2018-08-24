bc612d988cf42d98a00b83c158f2acd71cc09436
Ammar <ammar.alnatsheh@gmail.com>
2018-08-24 00:53:18 -0700

    modifiy queue

--- W1D5/Queue.rb
+++ W1D5/Queue.rb
@@ -1,19 +1,23 @@
 class Queue
   attr_reader :root
   def initialize
+  # create ivar to store queue here!
     @root = []
   end
 
   def enqueue(el)
+  # adds an element to the queue
     @root.push(el)
     el
   end
 
   def dequeue
+  # removes one element from the queue
     @root.shift
   end
 
   def peek
+  # returns, but doesn't remove, the first element in the queue
     @root[0]
   end
 
