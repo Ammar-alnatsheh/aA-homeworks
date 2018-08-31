50e683f7f6303fa79ab08596fe86175fd09e9fee
Ammar <ammar.alnatsheh@gmail.com>
2018-08-31 07:05:55 -0700

    W2D5 homework

---
+++ W2d5/LRUCatch.rb
@@ -0,0 +1,33 @@
+class LRUCache
+  def initialize(size)
+    @size = size
+    @arr = []
+  end
+
+  def count
+    # returns number of elements currently in cache
+    @arr.count
+  end
+
+  def add(el)
+    # adds element to cache according to LRU principle
+    if @arr.include?(el)
+      @arr.delete(el)
+      @arr << el
+    elsif count >= @size
+      @arr.shift
+      @arr << el
+    else
+      @arr << el
+    end
+  end
+
+  def show
+    # shows the items in the cache, with the LRU item first
+    @arr
+  end
+
+  private
+  # helper methods go here!
+
+end
